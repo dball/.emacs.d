0801c449460041976b1fc94ebae2545569ee8ad7
Donald Ball <donald.ball@gmail.com>
2019-04-09 10:19:21 -0400

    Add company mode

--- init.el
+++ init.el
@@ -14,13 +14,17 @@
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
 
+;; Enable company mode in clojure mode and cider nrepl mode
+(add-hook 'cider-mode-hook #'company-mode)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+
 ;; Wow it's annoying that a package stuffs its autogen config here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (paredit cider))))
+ '(package-selected-packages (quote (company paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
