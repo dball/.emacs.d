14a849e78af8a0cd8c24b0d8a935dc6fcab4f986
Donald Ball <donald.ball@gmail.com>
2019-04-09 10:28:11 -0400

    Enable dark mode

--- init.el
+++ init.el
@@ -9,16 +9,20 @@
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; Enable paredit mode in clojure mode and cider nrepl mode
+;; Enable paredit mode in clojure and cider
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'clojure-mode-hook #'enable-paredit-mode)
 (add-hook 'cider-repl-mode-hook #'enable-paredit-mode)
 
-;; Enable company mode in clojure mode and cider nrepl mode
+;; Enable company mode in clojure and cider
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'company-mode)
 
+;; Enable eldoc mode in cider
 (add-hook 'cider-mode-hook #'eldoc-mode)
+
+;; Dark, peaceful colors
+(load-theme 'zenburn t)
 
 ;; Wow it's annoying that a package stuffs its autogen config here
 (custom-set-variables
@@ -26,7 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company paredit cider))))
+ '(package-selected-packages (quote (zenburn-theme company paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
