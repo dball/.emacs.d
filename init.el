47c2bf2a3fe5eb535f0a3386416a555e5b7f53f0
Donald Ball <donald.ball@gmail.com>
2019-04-10 10:34:47 -0400

    Add projectile

--- init.el
+++ init.el
@@ -27,13 +27,17 @@
 ;; Dark, peaceful colors
 (load-theme 'zenburn t)
 
+;; Projectile mode for file loading?
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
 ;; Wow it's annoying that a package stuffs its autogen config here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (zenburn-theme company paredit cider))))
+ '(package-selected-packages (quote (projectile zenburn-theme company paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
