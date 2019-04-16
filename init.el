d758a838006a175c52457bbfeb9dcc66a41f94cd
Donald Ball <donald.ball@gmail.com>
2019-04-16 13:21:54 -0400

    Add inf-clojure mode

--- init.el
+++ init.el
@@ -41,7 +41,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (projectile zenburn-theme company paredit cider))))
+ '(package-selected-packages
+   (quote
+    (inf-clojure projectile zenburn-theme company paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
