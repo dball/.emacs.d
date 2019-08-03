a83d355edacc7eb12c6b878b97532761bc28a5e6
Donald Ball <donaldball@Donalds-MBP.attlocal.net>
2019-08-03 15:33:04 -0400

    Configure xref-js2

--- init.el
+++ init.el
@@ -61,10 +61,10 @@
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 (js2r-add-keybindings-with-prefix "C-c C-r")
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
-;;(require 'xref-js2)
+(require 'xref-js2)
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
-;;(define-key js-mode-map (kbd "M-.") nil)
+(define-key js-mode-map (kbd "M-.") nil)
 
-;;(add-hook 'js2-mode-hook (lambda ()
-;;  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
