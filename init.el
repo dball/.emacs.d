99b49c236001a932c96bf54086e5f73ffa4bb304
Donald Ball <donaldball@Donalds-MBP.attlocal.net>
2019-08-03 15:30:58 -0400

    Configure js2-refactor

--- init.el
+++ init.el
@@ -57,3 +57,14 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+(require 'js2-refactor)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+;;(require 'xref-js2)
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+;;(define-key js-mode-map (kbd "M-.") nil)
+
+;;(add-hook 'js2-mode-hook (lambda ()
+;;  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
