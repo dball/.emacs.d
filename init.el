717c605d3ae9a4a96e75f3a503bb7ab5e747f18c
Donald Ball <donald.ball@gmail.com>
2019-08-06 21:23:04 -0400

    Configure javascript indentation

--- init.el
+++ init.el
@@ -68,3 +68,9 @@
 
 (add-hook 'js2-mode-hook (lambda ()
   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+;; smartparens-mode
+;; show-smartparens-mode
+
+(setq-default js2-basic-offset 2)
+(setq-default indent-tabs-mode nil)
